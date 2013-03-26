d926cbbadab8aeac52a6948146644df2a877ad5c
PC_Gui <PC_Gui@PC_Gui-PC>
2013-03-26 02:14:07 -0300

    mensagem

--- src/br/calebe/exemplos/ex01/Carrinho.java
+++ src/br/calebe/exemplos/ex01/Carrinho.java
@@ -14,9 +14,15 @@ public class Carrinho {
 	public void add(Produto produto) {
 		produtos.add(produto);
 	}
+        
+        public void remove(Produto produto) throws CarrinhoVazioExpected {
+                if (produtos.isEmpty())
+			throw new CarrinhoVazioExpected();
+		produtos.remove(produto);
+	}
 
 	public Produto menorProduto() throws CarrinhoVazioExpected {
-		if (produtos.size() == 0)
+		if (produtos.isEmpty())
 			throw new CarrinhoVazioExpected();
 		Produto menor = produtos.get(0);
 		for (Produto produto : produtos) {
@@ -25,5 +31,35 @@ public class Carrinho {
 		}
 		return menor;
 	}
-
+        
+        public boolean buscarProduto(Produto p) throws CarrinhoVazioExpected {
+                if (produtos.isEmpty())
+			throw new CarrinhoVazioExpected();
+		for (Produto produto : produtos) {
+			if (p.equals(produto))
+				return true;
+		}
+		return false;
+        }
+        
+        public int numeroDeProdutos(){
+                return produtos.size();
+        }
+        
+        public void listarProdutos() throws CarrinhoVazioExpected{
+                if (produtos.isEmpty())
+			throw new CarrinhoVazioExpected();
+                System.out.println("*********Lista de Produtos**********");
+		for (Produto produto : produtos) 
+			System.out.println("Produto: " + "\"" + produto.getNome() + "\"" +  "      Valor: " + produto.getPreco() + "\n");
+        }
+        
+        public double calcularTotalPedido(){
+               double total = 0.0;
+               if (produtos.isEmpty())
+                    return 0.0;
+               for (Produto produto : produtos) 
+                    total += produto.getPreco();
+               return total;
+        } 
 }
